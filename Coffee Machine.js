44ec2c13a82866a63506dc7fa1fbc8a43971b254
Luis Andrino <67714289+LuisAndrino@users.noreply.github.com>
2023-01-13 13:00:07 -0600

    Stage 3/6 complete
    Now you can add data.

--- Coffee Machine.js
+++ Coffee Machine.js
@@ -16,22 +16,16 @@ let milk = Number(input("Write how many ml of milk the coffee machine has:"));
 let coffeeBeans = Number(input("Write how many grams of coffee beans the coffee machine has:"));
 
 console.log("Write how many cups of coffee you will need:")
-let coffee = input();
-if(water > 200 && milk > 20 && coffeeBeans > 15){
-  if(water % 200 == 0 && milk % 50 == 0 && coffeeBeans % 15 == 0 ){
-    water = water / 200;
-    milk = milk / 50;
-    coffeeBeans = coffeeBeans / 15;
-    let findMin = Math.min(water, milk, coffeeBeans);
-    
-    if(num > findMin){
-      console.log(`No, I can make only ${findMin} cups of coffee`);
-    }
-    else if(num === findMin){
-      console.log("Yes, I can make that amount of coffee");
-    }
-    else if(num < findMin){
-      console.log(`Yes, I can make that amount of coffee (and even ${findMin - num} more than that)`);
-    }
-  }
+let coffee = Number(input());
+water = water / 200;
+milk = milk / 50;
+coffeeBeans = coffeeBeans / 15;
+let findMin = Math.trunc(Math.min(water, milk, coffeeBeans));
+
+if (coffee > findMin) {
+    console.log(`No, I can make only ${findMin} cups of coffee`);
+} else if (coffee === findMin) {
+    console.log("Yes, I can make that amount of coffee");
+} else if (coffee < findMin) {
+    console.log(`Yes, I can make that amount of coffee (and even ${findMin - coffee} more than that)`);
 }
